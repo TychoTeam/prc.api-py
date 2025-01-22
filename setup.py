1ae36aae72be7c6d403c274e4819db64988f96ac
ManHatos <omar@batheeb.net>
2025-01-22 19:28:20 +0300

    add framework

--- setup.py
+++ setup.py
@@ -18,5 +18,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Framework :: AsyncIO",
     ],
 )
