3abaf094a25e756b97d0e58d5d903ba2ee19d60b
ManHatos <omar@batheeb.net>
2025-01-22 17:16:30 +0300

    add missing model exports

--- prc/models/__init__.py
+++ prc/models/__init__.py
@@ -5,9 +5,16 @@ Classes to parse and transform PRC API data.
 """
 
 from .server.status import ServerStatus, ServerOwner, AccountRequirement
-from .server.player import ServerPlayer, QueuedPlayer, PlayerPermission
+from .server.player import ServerPlayer, QueuedPlayer, PlayerPermission, PlayerTeam
 from .server.vehicle import Vehicle, VehicleName, VehicleModel, VehicleOwner
-from .server.commands import Command, CommandArg, CommandName, FireType, Weather
+from .server.commands import (
+    Command,
+    CommandArg,
+    CommandName,
+    FireType,
+    Weather,
+    CommandTarget,
+)
 from .server.logs import (
     LogEntry,
     LogPlayer,
