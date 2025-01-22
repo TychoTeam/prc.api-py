9dbc9512e3cbb5c32748cab6bc244a9742380d30
ManHatos <omar@batheeb.net>
2025-01-23 00:23:46 +0300

    v0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="prc.api",
-    version="0.1.1",
+    version="0.1.2",
     author="tycho",
     url="https://github.com/Tycho-Systems/prc.api",
     license="MIT",
