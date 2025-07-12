30fcf64667b686a239714bb21fcbda5c0837779b
ManHat <omar@batheeb.net>
2025-07-13 01:25:09 +0300

    update models init

--- prc/models/__init__.py
+++ prc/models/__init__.py
@@ -4,8 +4,14 @@ Classes to parse and transform PRC API data.
 
 """
 
-from .server.status import ServerStatus, ServerOwner, AccountRequirement
-from .server.player import ServerPlayer, QueuedPlayer, PlayerPermission, PlayerTeam
+from .server.status import ServerStatus, AccountRequirement
+from .server.player import (
+    ServerPlayer,
+    ServerOwner,
+    QueuedPlayer,
+    PlayerPermission,
+    PlayerTeam,
+)
 from .server.vehicle import Vehicle, VehicleName, VehicleModel, VehicleOwner
 from .server.logs import (
     LogEntry,
